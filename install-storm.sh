1e7da0a659bbcf7774f8786ea2607df5ff9151a7
Andy Cobley <andycobley@andys-imac-2.computing.dundee.ac.uk>
2014-05-23 13:29:36 +0100

    moved jars to storm/lib

--- install-storm.sh
+++ install-storm.sh
@@ -16,8 +16,10 @@ chown storm:storm /etc/storm
 
 gunzip /vagrant/$2.tar.gz
 tar xvf /vagrant/$2.tar -C /usr/share/java
-mv /usr/share/java/$2/*.jar /usr/share/java
-mv /usr/share/java/cassandra-java-driver-2.0.2/lib/*.jar /usr/share/java
+mv /usr/share/java/$2/*.jar /usr/share/storm/lib
+mv /usr/share/java/$2/lib/*.jar /usr/share/storm/lib
+rm /usr/share/apache-storm-0.9.1-incubating/lib/slf4j-api-1.7.5.jar
+rm /usr/share/apache-storm-0.9.1-incubating/lib/slf4j-log4j12-1.7.6.jar
 rm /usr/share/storm/conf/storm.yaml
 cp /vagrant/storm.yaml /usr/share/storm/conf/
 cp /vagrant/cluster.xml /usr/share/storm/logback/
@@ -26,6 +28,6 @@ ln -s /usr/share/storm/conf/storm.yaml /etc/storm/storm.yaml
 mkdir /var/log/storm
 chown storm:storm /var/log/storm
 
-//echo "export CLASSPATH=\$CLASSPATH:/usr/share/java" >> /etc/profile
+#echo "export CLASSPATH=\$CLASSPATH:/usr/share/java" >> /etc/profile
 
 #sed -i 's/${storm.home}\/logs/\/var\/log\/storm/g' /usr/share/storm/logback/cluster.xml
